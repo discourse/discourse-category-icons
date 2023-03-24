ac6122231b4cdeac1756a9356c6d06d259f19a3a
awesomerobot <kris.aubuchon@discourse.org>
2023-03-24 16:00:54 -0400

    DEV: add component for cross-theme compatibility

---
+++ javascripts/discourse/components/category-icon.js
@@ -0,0 +1,16 @@
+/* This component is used in the Category banners theme component */
+/* https://meta.discourse.org/t/category-banners/86241 */
+import MountWidget from "discourse/components/mount-widget";
+
+export default class CategoryIcon extends MountWidget {
+  widget = "category-icon";
+
+  buildArgs() {
+    return { category: this.category };
+  }
+
+  didReceiveAttrs() {
+    super.didReceiveAttrs();
+    this.queueRerender();
+  }
+}
